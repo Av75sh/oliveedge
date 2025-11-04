c737b846395d2a9ca202549d0da480398d42018a
Av75sh <avinashlpu75@gmail.com>
2025-11-04 08:29:07 +0530

    render5

--- oliveedge/settings.py
+++ oliveedge/settings.py
@@ -89,7 +89,7 @@ STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 
 # Media files (won't work on Vercel - use cloud storage for production)
 MEDIA_URL = '/static/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = BASE_DIR / 'static' / 'media'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
